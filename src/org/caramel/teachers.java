77ccc537bff096bf34e63a3a58961e00ddc8a9e8
shamjithchandran <shamjithchandran005@gmail.com>
2022-03-30 10:25:30 +0530

    new priject added

---
+++ src/org/caramel/teachers.java
@@ -0,0 +1,14 @@
+package org.caramel;
+
+public class teachers {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("students are100");
+		System.out.println("teachers are 30"); 
+		
+		
+		
+	}
+
+}
